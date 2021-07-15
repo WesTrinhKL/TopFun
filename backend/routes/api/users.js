3538ebc2092c8e170c71cf2033cbbbcbe35e9b7e
wes <trinhpt@uci.edu>
2021-07-15 15:29:21 -0700

    signup user

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,8 +1,24 @@
-//@hold resources for routes starting with: /api/users
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
+//@sign up route
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password }); //call static method in User model to sign up
 
+    await setTokenCookie(res, user);
 
+    return res.json({
+      user,
+    });
+  }),
+);
 
 module.exports = router;
