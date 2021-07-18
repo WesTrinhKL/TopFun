0b7f405d2c22b4a3cdadbd2e543e7a7beab7eb8b
wes <trinhpt@uci.edu>
2021-07-17 22:29:44 -0700

    providers for app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+//wrap our app with the store and router providers
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
