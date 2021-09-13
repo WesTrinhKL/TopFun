c225337bd755869918f9f7449df6fed8cf247969
wes <trinhpt@uci.edu>
2021-09-12 22:47:17 -0700

    updated carousel

--- frontend/src/components/ListCarousel/index.js
+++ frontend/src/components/ListCarousel/index.js
@@ -1,4 +1,4 @@
-import  React from 'react'
+import  React, {useEffect, useRef, useState} from 'react'
 import { Link, useHistory} from 'react-router-dom';
 
 
@@ -11,6 +11,32 @@ export const ListCarousel = (props) => {
     history.push(`/view-list/${props.id}`)
   }
 
+  const scrollEl = useRef();
+  const [incrementVal, setIncrementVal ] = useState(650);
+  const [scrollPos, setScrollPos] = useState(0);
+  const [scrollMax, setScrollMax] = useState(props.listItemsArray.length * 200 - 742 - 5) //length of the whole list minus the container and margin will then yield the remaining list max width that is currently 'invisible'
+
+  useEffect(() => {
+    scrollEl.current.scrollLeft = 0;
+  }, [])
+
+  const scrollLogicHandler = (direction)=>{
+    let inc = incrementVal;
+    let pos = scrollPos;
+
+    // Reduce increment if pos at a container edge
+    if (pos === 0 || pos === scrollMax) inc -= 34;
+
+    if (direction === "left") inc *= -1;
+    pos += inc;
+
+    scrollEl.current.scrollLeft = pos;
+    setScrollPos(Math.max(0, Math.min(pos, scrollMax)));
+  }
+
+  let leftClass = (scrollPos !== 0) ? "discover-button-container" : "discover-button-container";
+  let rightClass = (scrollPos !== scrollMax) ? "discover-button-container" : "discover-button-container ";
+
 
   return (
     <div className="full-item-wrapper">
@@ -19,9 +45,27 @@ export const ListCarousel = (props) => {
         <div className="author-title-homepage">Made by: {props.userObj.username}</div>
         {/* <img className="image-cover" src={props.coverPhotoLink} alt="img" /> */}
         <div className="content-container-homepage">
-          {props.listItemsArray.map(listItem=>(
-            <img key={listItem.id} className="image-cover" src={listItem.imageLink} alt="img" />
-          ))}
+          <div ref={scrollEl} className="cch__carousel-list">
+
+            <div className="carousel-list__scroll-container">
+              {/* duvdd */}
+              <div className={leftClass} onClick={() => scrollLogicHandler("left")}>
+                <button className="button-carousel" onClick={() => scrollLogicHandler("left")}><i class="fas fa-angle-left"></i></button>
+              </div>
+
+              <div className={rightClass} onClick={() => scrollLogicHandler("right")}>
+                <button className="button-carousel" onClick={() => scrollLogicHandler("right")}><i class="fas fa-angle-right"></i></button>
+              </div>
+            </div>
+
+            {/* carousel items */}
+            {props.listItemsArray.map(listItem=>(
+              <img key={listItem.id} className="image-cover" src={listItem.imageLink} alt="img" />
+            ))}
+          </div>
+
+
+
           <div className="list-view-button">
             <Link to={`/view-list/${props.id}`}  className="button-style-list"href="">view full list</Link>
           </div>
