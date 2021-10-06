87848f472f72dd5c18f03701747d5f5b64c2e330
wes <trinhpt@uci.edu>
2021-10-06 16:13:50 -0700

    fixed seed data

--- backend/db/seeders/21210722031201-seed-lists.js
+++ backend/db/seeders/21210722031201-seed-lists.js
@@ -27,7 +27,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        title: "My Top 5 Active NBA Players",
+        title: "My Top 10 Active NBA Players",
         coverPhotoLink: "https://topfunphotos.s3.us-west-1.amazonaws.com/topfunimages/nbaplayers/ANTETOKOUNMPO_G_USATSI_14878894.jpg",
         description: "This is the TRUTH, this is straight factz, no cap",
         categoryId: 3,
